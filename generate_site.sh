40318b47b2d48a0b82760005a65543016b7ac20e
chatasma <nodeadbatteries@gmail.com>
2020-11-10 23:33:53 -0600

    Add more content

--- generate_site.sh
+++ generate_site.sh
@@ -2,7 +2,7 @@
 
 base_dir="$(pwd)"
 target_dir="$(pwd)/public"
-target_content="${target_dir}/content"
+target_content="${target_dir}/notes"
 
 if [ -d $target_dir ]; then
     rm -r $target_dir
@@ -11,24 +11,21 @@ fi
 mkdir $target_dir 
 mkdir $target_content
 
-course_list="<ul>\n"
-
 cd content
 for semester in */ ; do
     cd ${semester}
-    mkdir "${target_content}/${semester}"
     echo "Semester: ${semester}"
     echo "----------"
     for course in */ ; do
         cd ${course}
-        mkdir "${target_content}/${semester}/${course}"
+        mkdir "${target_content}/${course}"
         echo ""
         echo "Course: ${course}"
         echo "----------"
         echo ""
         for topic in */ ; do
             cd ${topic}
-            mkdir "${target_content}/${semester}/${course}/${topic}"
+            mkdir "${target_content}/${course}/${topic}"
             echo "Compiling ${topic}..."
             make html &> /dev/null
             if [ $? -ne 0 ]; then
@@ -36,23 +33,19 @@ for semester in */ ; do
                 exit 1
             else
                 echo "Compiled successfully"
-                cp "./out/index.html" "${target_content}/${semester}/${course}/${topic}/"
+                cp "./out/index.html" "${target_content}/${course}/${topic}/"
             fi
             cd ..
         done
-        course_list="${course_list}  <li>${course}</li>\n"
         cd ..
     done
     cd ..
 done
 
-course_list="${course_list}</ul>"
 welcome_page="${target_dir}/index.html"
-
-echo -en $course_list > .tmp_1
-course_list_file=$(realpath .tmp_1)
-
 cd $base_dir
 
+cp ./templates/welcome.html ${welcome_page}
+
 # Main page
-node build/ ./templates/welcome.html ${course_list_file} ${welcome_page}
+node build/ ./content/ ./public/ ./templates/
